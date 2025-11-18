ca544f7de29d30f9dfe3a6c3fecdb44baff95239
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2025-11-18 16:34:51 -0400

    pokemons

--- app.js
+++ app.js
@@ -20,6 +20,23 @@ app.get('/tico', (req, res) => {
     res.send("teco");
 });
 
+app.get('/pokemons', (req, res) => {
+    const pokemons = [
+        "Pikachu",
+        "Caterpie",
+        "Pidgeotto",
+        "Bulbasaur",
+        "Charmander",
+        "Squirtle",
+        "Krabby",
+        "Muk",
+        "Kingler",
+        "Primeape"
+    ];
+
+    res.json(pokemons);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
